feccf2b94eb85122261db3fa6426eedaaeb7ffe4
Malanchenko Sergii <malan.sergii@gmail.com>
2023-07-17 15:17:30 +0300

    remove all

--- src/components/app/App1.js
+++ src/components/app/App1.js
@@ -1,16 +1,5 @@
-import EventBoard from "components/eventBoard";
-import PageTitle from "components/pageTitle";
-import { Container } from "./App1.styled";
-
-import events from "../../data/upcomingEvents.json";
-
 const App = () => {
-    return (
-        <Container>
-            <PageTitle text="Page Title"></PageTitle>
-            <EventBoard events={events}></EventBoard>
-        </Container>
-    );
+    return <></>;
 };
 
 export default App;
